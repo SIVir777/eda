0ea6a0868b0a19b9e283d32195ed5f15830a6df2
unknown <ilyaka4ook@gmail.com>
2022-04-24 19:59:35 +0300

    that-commit-is-push-in-gh-pages

--- src/components/UI/index.js
+++
@@ -1,7 +0,0 @@
-import MyInput from '@/components/UI/MyInput';
-import ButtonModal from '@/components/UI/ButtonModal';
-import Loader from '@/components/UI/Loader';
-
-export default [ 
-    MyInput, ButtonModal, Loader,
-];
